5ee94921258c4a6fcf01b1b6a9d0f7e4f0f5ef01
董航 <17816890438@163.com>
2020-04-25 14:09:57 +0800

    1

---
+++ src/a.java
@@ -0,0 +1,11 @@
+/**
+ * @Description:
+ * @Author: DongHang
+ * @Date: 2020-04-25 14:08
+ * @Content:
+ */
+public class a {
+    public static void main(String[] args) {
+        System.out.println(123);
+    }
+}
